4bb63afafedcca9196bb1d5d5e25c95aab38054a
renevds <rene.v.d.s.9@gmail.com>
2021-01-18 16:10:56 +0100

    Server post

--- src/main/java/chava/ChavaHttpHandler.java
+++ src/main/java/chava/ChavaHttpHandler.java
@@ -4,42 +4,62 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.commons.text.StringEscapeUtils;
 
 public class ChavaHttpHandler implements HttpHandler {
 
+    private MessageController msgCtrl;
+
+    public ChavaHttpHandler(MessageController msgCtr){
+        this.msgCtrl = msgCtr;
+    }
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         System.out.println(httpExchange.getRemoteAddress() + " connected. (" + httpExchange.getRequestMethod() + ")");
-        String requestParamValue = null;
         if ("GET".equals(httpExchange.getRequestMethod())) {
-            handleResponse(httpExchange, "");
-        } else if ("POST".equals(httpExchange)) {
+            handleResponse(httpExchange, msgCtrl.displayMessages());
+        } else if ("POST".equals(httpExchange.getRequestMethod())) {
             handlePostRequest(httpExchange);
         }
 
     }
 
-    private String handlePostRequest(HttpExchange httpExchange) {
-        System.out.println(httpExchange.getRequestBody().toString());
-        return httpExchange.getRequestBody().toString();
+    private void handlePostRequest(HttpExchange httpExchange) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStream ios = httpExchange.getRequestBody();
+        int i;
+        while ((i = ios.read()) != -1) {
+            sb.append((char) i);
+        }
+
+        String messageString = sb.toString().substring(8);
+
+
+
+        System.out.println("hm: " + messageString);
+        handleResponse(httpExchange, msgCtrl.displayMessages());
     }
 
     private void handleResponse(HttpExchange httpExchange, String requestParamValue)  throws  IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
         StringBuilder htmlBuilder = new StringBuilder();
         htmlBuilder.append("<h1>Welkom ").append(httpExchange.getRemoteAddress().toString()).append("</h1>");
+        htmlBuilder.append(requestParamValue);
+        htmlBuilder.append("<form action=\"\" method=\"post\">\n" +
+                "  <label for=\"message\">Message:</label><br>\n" +
+                "  <input type=\"text\" id=\"message\" name=\"message\"><br>\n" +
+                "<input type=\"submit\" value=\"Submit\">" +
+                "</form>");
         httpExchange.sendResponseHeaders(200, htmlBuilder.toString().length());
         outputStream.write(htmlBuilder.toString().getBytes());
         outputStream.flush();
         outputStream.close();
     }
 
-    private String handleGetRequest(HttpExchange httpExchange) {
-        return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
-    }
 
 
 }
--- src/main/java/chava/ChavaServer.java
+++ src/main/java/chava/ChavaServer.java
@@ -18,9 +18,14 @@ public class ChavaServer {
     }
 
     public void startServer() throws IOException {
+        MessageController msgCtrl = new MessageController();
+        Person testperson = new Person("test_ip");
+        testperson.setNickname("tester");
+        msgCtrl.addMessage(new Message("test", testperson));
+
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
         server = HttpServer.create(new InetSocketAddress(ip, port), 0);
-        server.createContext("/", new  ChavaHttpHandler());
+        server.createContext("/", new  ChavaHttpHandler(msgCtrl));
         server.setExecutor(threadPoolExecutor);
         server.start();
         System.out.println("Server started on " + ip + "@" + port);
